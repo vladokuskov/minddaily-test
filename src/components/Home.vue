74c5cd49a0a0ab46def83168baaea86f9225931b
Vladyslav Okuskov <vokuskov@artelogic.net>
2024-06-08 18:54:17 +0300

    TEST

---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <button @click="handleGoogle">Google</button>
+ <div>Home</div>
+</template>
+
+<script setup lang="ts">
+
+const handleGoogle = () => {
+const baseUrl = "http://minddaily.joshibbotson.com:8082";
+const redirectUri = `${baseUrl}/auth/google`;
+window.location.href = redirectUri;
+}
+</script>
+
